66921fb1fd12c006b7fa64d3e2f1171ef1731dfe
AJ <necessarylion@gmail.com>
2023-04-22 18:18:38 +0700

    test worlflow

--- test/sql_query_builder_test.dart
+++ test/sql_query_builder_test.dart
@@ -3,15 +3,24 @@ import 'package:sql_query_builder/sql_query_builder.dart';
 import 'package:test/test.dart';
 
 void main() {
-  PostgreSQLConnection db = PostgreSQLConnection("localhost", 5432, "postgres",
-      username: "admin", password: "password");
+  PostgreSQLConnection db = PostgreSQLConnection(
+    "localhost",
+    5432,
+    "postgres",
+    username: "admin",
+    password: "password",
+  );
   var queryBuilder = QueryBuilder(db);
 
   group('A group of tests', () {
     setUp(() {});
 
     test('First Test', () async {
-      var admin = await queryBuilder.debug(true).table('admin').find(1);
+      var admin = await queryBuilder
+          .debug(true)
+          .table('admin')
+          .join('admin_info', 'admin_info.admin_id', 'admin.id')
+          .get();
       print(admin);
     });
   });

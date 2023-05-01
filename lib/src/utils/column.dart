fcf7227e3f79397695d9f3186951a289558fcfdb
AJ <aj.zinkyaw@gmail.com>
2023-05-01 18:23:07 +0700

    bug fixed on column and isModel instance

--- lib/src/utils/column.dart
+++ lib/src/utils/column.dart
@@ -1,16 +1,9 @@
 import 'package:json_annotation/json_annotation.dart';
 
 class Column extends JsonKey {
-  @override
-  final String? name;
-
-  // final Function(dynamic)? filter;
-  const Column({this.name});
+  const Column({name}) : super(name: name);
 }
 
 class IsModel extends JsonSerializable {
-  @override
-  final bool ignoreUnannotated;
-
-  const IsModel({this.ignoreUnannotated = true});
+  const IsModel() : super(ignoreUnannotated: true);
 }

976f30ce3afd44969bd3c701aeee0aedd8d92963
AJ <aj.zinkyaw@gmail.com>
2023-06-05 00:07:39 +0700

    check with localhost

--- test/connection.dart
+++ test/connection.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 import 'package:postgres_pool/postgres_pool.dart';
 
-connection() async {
-  String host = Platform.environment['DB_HOST'] ?? 'postgres';
+Future<PostgreSQLConnection> connection() async {
+  String host = Platform.environment['DB_HOST'] ?? 'localhost';
   int port = int.parse(Platform.environment['PORT'] ?? '5432');
   PostgreSQLConnection db = PostgreSQLConnection(
     host,
@@ -16,8 +16,8 @@ connection() async {
   return db;
 }
 
-poolConnection() {
-  String host = Platform.environment['DB_HOST'] ?? 'postgres';
+PgPool poolConnection() {
+  String host = Platform.environment['DB_HOST'] ?? 'localhost';
   int port = int.parse(Platform.environment['PORT'] ?? '5432');
   return PgPool(
     PgEndpoint(
